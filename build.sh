d0b03f3ad6d233c056fbd0e71ffe3260f464342b
Seb Zanardo <sebzanardo@optusnet.com.au>
2025-03-28 14:16:39 +1100

    Added multi-platform build.sh script. Created empty src/resources folder that should include all game assets

--- build.sh
+++ build.sh
@@ -1,11 +1,76 @@
-# The simplest possible build command (raylib->GNU Linux)
-# https://github.com/raysan5/raylib/wiki/Working-on-GNU-Linux
-cc src/main.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -o game.exe
+#!/bin/sh
+
+
+LINUX="linux"
+MACOS="macos"
+WINDOWS="windows"
+WEB="web"
+
+
+help() {
+    echo "Please specify a valid target platform:"
+    echo "$0 $LINUX"
+    echo "$0 $MACOS"
+    echo "$0 $WINDOWS"
+    echo "$0 $WEB"
+    exit 1
+}
+
+help_web() {
+    echo "Please source emsdk before trying to build for $WEB"
+    echo ". \$HOME/emsdk/emsdk_env.sh"
+    exit 1
+}
+
+
+if [ -z $1 ]; then
+    help
+
+elif [ $1 = $LINUX ]; then
+    # https://github.com/raysan5/raylib/wiki/Working-on-GNU-Linux
+    cc src/main.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -o game.exe
+
+elif [ $1 = $MACOS ]; then
+    # https://github.com/raysan5/raylib/wiki/Working-on-macOS
+    eval cc src/main.c -framework IOKit -framework Cocoa -framework OpenGL $(pkg-config --libs --cflags raylib) -o game.exe
+
+elif [ $1 = $WINDOWS ]; then
+    # https://github.com/raysan5/raylib/wiki/Working-on-Windows
+    gcc src/main.c -lraylib -lgdi32 -lwinmm -o game.exe
+
+elif [ $1 = $WEB ]; then
+    if [ -z "$EMSDK" ]; then
+        help_web
+    fi
+
+    # Try make web directory if doesn't exist
+    mkdir -p web
+
+    # https://github.com/raysan5/raylib/wiki/Working-for-Web-(HTML5)v.sh
+    emcc -o web/game.html src/main.c -Os -Wall $HOME/raylib/src/web/libraylib.web.a \
+        -I. -I$HOME/raylib/src -L. -L$HOME/raylib/src/web \
+        -s USE_GLFW=3 \
+        -s ASYNCIFY \
+        --shell-file $HOME/raylib/src/minshell.html \
+        --preload-file src/resources \
+        -s TOTAL_STACK=64MB \
+        -s INITIAL_MEMORY=128MB \
+        -DPLATFORM_WEB
+
+else
+    help
+fi
 
 # Exit the script if the last command was unsuccessful
 if [ $? -ne 0 ]; then
-	exit 1
+    echo "Failed to build"
+    exit 1
 fi
 
-# Run the executable that was created
-./game.exe
+if [ $1 = $WEB ]; then
+    # Run web version
+    emrun web/game.html
+else
+    # Run the executable that was created
+    ./game.exe
+fi
